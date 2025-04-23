acbd94174443f31f7ec030ded9fd8a3d32929959
VictorKaue <victorkaue909@gmail.com>
2025-04-23 09:50:34 -0300

    Initial Commit

---
+++ To-do List/To_do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_do_ListApp.swift
+//  To-do List
+//
+//  Created by Victor Kaue Lima De Paiva on 23/04/25.
+//
+
+import SwiftUI
+
+@main
+struct To_do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ To-do ListTests/To_do_ListTests.swift
@@ -0,0 +1,17 @@
+//
+//  To_do_ListTests.swift
+//  To-do ListTests
+//
+//  Created by Victor Kaue Lima De Paiva on 23/04/25.
+//
+
+import Testing
+@testable import To_do_List
+
+struct To_do_ListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
